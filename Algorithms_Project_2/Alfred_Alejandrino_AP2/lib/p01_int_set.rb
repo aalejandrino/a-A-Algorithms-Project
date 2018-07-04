f76a5b67ceef2519983e7fc22aaa1a2b575d38e8
AdoboFrenzy <alejandrino.alfred@gmail.com>
2018-07-03 21:48:36 -0700

    finish p01

--- Algorithms_Project_2/Alfred_Alejandrino_AP2/lib/p01_int_set.rb
+++ Algorithms_Project_2/Alfred_Alejandrino_AP2/lib/p01_int_set.rb
@@ -1,22 +1,30 @@
 class MaxIntSet
   def initialize(max)
+    @max = max
+    @store = Array.new(max) {false}
   end
 
   def insert(num)
+    is_valid?(num)
+    @store[num] = true
   end
 
   def remove(num)
+    is_valid?(num)
+    @store[num] = false
   end
 
   def include?(num)
+    return @store[num]
   end
 
   private
 
   def is_valid?(num)
+    raise "Out of bounds" if (num < 0 || num > @max)
   end
 
-  def validate!(num)
+  def validate!(num) 
   end
 end
 
@@ -24,21 +32,26 @@ end
 class IntSet
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
+    @num_buckets = num_buckets
+  
   end
 
   def insert(num)
+    self[num] << num 
   end
 
   def remove(num)
+    self[num].delete(num) if include?(num)
   end
 
   def include?(num)
+    self[num].any? {|n| n == num}
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -47,26 +60,32 @@ class IntSet
 end
 
 class ResizingIntSet
-  attr_reader :count
+  attr_reader :count, :num_buckets
 
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
+    @num_buckets = num_buckets
     @count = 0
   end
 
   def insert(num)
+    resize! if count == num_buckets
+    self[num] << num
+    @count += 1
   end
 
   def remove(num)
+    self[num].delete(num) if include?(num)
   end
 
   def include?(num)
+    self[num].any? {|n| n == num}
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -74,5 +93,14 @@ class ResizingIntSet
   end
 
   def resize!
+    @num_buckets *= 2
+    newStore = Array.new(@num_buckets) {Array.new}
+
+    @store.each do |bucket|
+      bucket.each {|el| newStore[el % @num_buckets] << el}
+    end
+
+    @store = newStore
+
   end
 end
