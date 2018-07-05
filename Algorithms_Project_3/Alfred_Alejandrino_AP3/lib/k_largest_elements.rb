279c942488ea72e47bcc984fcafc1bc9e01b8e81
AdoboFrenzy <alejandrino.alfred@gmail.com>
2018-07-05 10:29:14 -0700

    add another soln using backwards heapsort

--- Algorithms_Project_3/Alfred_Alejandrino_AP3/lib/k_largest_elements.rb
+++ Algorithms_Project_3/Alfred_Alejandrino_AP3/lib/k_largest_elements.rb
@@ -15,8 +15,28 @@ def k_largest_elements(array, k)
 
     # return result
     
-    ## above solution uses BinaryMinHeap and extracts max
-    ## below solution uses HeapSort!
+    ## above solution uses BinaryMinHeap and extracts max <<
+    ## below solution uses HeapSort! <<
 
-    return array.heap_sort!.drop(array.length - k)
+    # return array.heap_sort!.drop(array.length - k)
+
+
+    ## below solution uses a backwards heapsort!
+    prc = Proc.new {|el1, el2| (el1 <=> el2)}
+    arr_len = array.length
+
+    (1...arr_len).each do |idx|
+      BinaryMinHeap.heapify_up(array, idx, &prc)
+    end
+
+    array[0], array[-1] = array[-1], array[0]
+  
+    (arr_len-2).downto(0).each do |idx|
+      BinaryMinHeap.heapify_down(array, 0, idx+1, &prc)
+      array[0], array[idx] = array[idx], array[0]
+      
+    end
+
+    # p array
+    return array.take(k)
 end
