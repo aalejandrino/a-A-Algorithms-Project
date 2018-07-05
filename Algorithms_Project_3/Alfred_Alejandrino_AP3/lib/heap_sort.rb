f898db205dff379161650a282eb3cc5dc2acb59e
AdoboFrenzy <alejandrino.alfred@gmail.com>
2018-07-05 09:55:15 -0700

    finish heap sort

--- Algorithms_Project_3/Alfred_Alejandrino_AP3/lib/heap_sort.rb
+++ Algorithms_Project_3/Alfred_Alejandrino_AP3/lib/heap_sort.rb
@@ -2,5 +2,24 @@ require_relative "heap"
 
 class Array
   def heap_sort!
+    # self.sort! #lol
+ 
+    prc = Proc.new {|el1, el2| (el2 <=> el1)}
+    arr_len = self.length
+
+    (1...arr_len).each do |idx|
+      BinaryMinHeap.heapify_up(self, idx, &prc)
+    end
+
+    self[0], self[-1] = self[-1], self[0]
+  
+    (arr_len-2).downto(0).each do |idx|
+      BinaryMinHeap.heapify_down(self, 0, idx+1, &prc)
+      self[0], self[idx] = self[idx], self[0]
+      
+    end
+
+    return self
+
   end
 end
