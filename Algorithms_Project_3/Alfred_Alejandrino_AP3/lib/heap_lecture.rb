f90e851472134164e668c80431d22a2e2a23e27b
AdoboFrenzy <alejandrino.alfred@gmail.com>
2018-07-06 14:41:03 -0700

    begin algorithms proj 5

--- Algorithms_Project_3/Alfred_Alejandrino_AP3/lib/heap_lecture.rb
+++ Algorithms_Project_3/Alfred_Alejandrino_AP3/lib/heap_lecture.rb
@@ -81,3 +81,21 @@ def almost_sorted(arr, k)
         heap.push(arr.shift) if arr[0]
     end
 end
+
+require_relative 'heap'
+
+def almost_sorted(arr, k)
+  heap = BinaryMinHeap.new
+  # If k = 2, the first output element must be
+  # within the first 3 numbers, so we build a heap of 3
+  (k + 1).times do
+    heap.push(arr.shift)
+  end
+
+  # Accounts for when the array runs out but we still have
+  # numbers in our heap
+  while heap.count > 0
+    print heap.extract
+    heap.push(arr.shift) if arr[0]
+  end
+end
