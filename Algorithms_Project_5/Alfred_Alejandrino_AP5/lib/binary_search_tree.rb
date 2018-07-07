fa550e99e00fdbad90caf8cfb25e13bbb4a7c3de
AdoboFrenzy <alejandrino.alfred@gmail.com>
2018-07-07 00:39:22 -0700

    work on AP5 problems

--- Algorithms_Project_5/Alfred_Alejandrino_AP5/lib/binary_search_tree.rb
+++ Algorithms_Project_5/Alfred_Alejandrino_AP5/lib/binary_search_tree.rb
@@ -1,31 +1,80 @@
 # There are many ways to implement these methods, feel free to add arguments 
 # to methods as you see fit, or to create helper methods.
 
+require_relative 'bst_node'
+
 class BinarySearchTree
+  attr_accessor :root
+
   def initialize
+    @root = nil
+    @count = 0
   end
 
   def insert(value)
+    newNode = BSTNode.new(value)
+
+    if @root == nil
+      @root = newNode
+    else
+      @root.set(newNode)
+    end
+
+    @count += 1
   end
 
   def find(value, tree_node = @root)
+    tree_node.find(value)
   end
 
   def delete(value)
+    node_to_del = find(value)
+    if (node_to_del.no_children? && node_to_del.no_parent?) #this is the root node
+      @root = nil 
+    elsif (node_to_del.no_children?) #has a parent node therefore, not the root node
+      node_to_del.parent.left, node_to_del.parent.right = nil, nil
+      node_to_del.parent = nil
+    elsif (node_to_del.one_child?)
+      parent = node_to_del.parent
+      parent.left = nil if value < parent.value
+      parent.right = nil if value > parent.value
+
+      parent.set(node_to_del.left || node_to_del.right)
+    else
+      parent = node_to_del.parent
+      max_node = maximum(node_to_del.left)
+      delete(max_node.value)
+
+      parent.left = nil if value < parent.value
+      parent.right = nil if value > parent.value
+
+      parent.set(max_node)
+    end
+
+    @count -= 1
+    
   end
 
-  # helper method for #delete:
+  # helper method   for #delete:
   def maximum(tree_node = @root)
+    curr_node = tree_node
+    curr_node = curr_node.right until curr_node.right == nil
+
+    return curr_node
   end
 
   def depth(tree_node = @root)
+    Math.log2(@count).ceil
   end 
 
   def is_balanced?(tree_node = @root)
+
   end
 
   def in_order_traversal(tree_node = @root, arr = [])
+    
   end
+
 
 
   private
--- Algorithms_Project_5/Alfred_Alejandrino_AP5/lib/bst_node.rb
+++ Algorithms_Project_5/Alfred_Alejandrino_AP5/lib/bst_node.rb
@@ -1,4 +1,66 @@
 class BSTNode
+  attr_accessor :value, :left, :right, :parent
+
   def initialize(value)
+    @value = value
+    @left = nil
+    @right = nil
+    @parent = nil
   end
+
+  def set(node)
+
+    if (node.value < @value)
+      !!@left ? @left.set(node) : (@left = node; node.parent = self)
+    elsif (@value < node.value)
+      !!@right ? @right.set(node) : (@right = node; node.parent = self)
+    end
+
+    # node.parent = self
+  end
+
+  def find(val)
+    return self if val == self.value
+
+    if !!@left && val < @value
+      @left.find(val)
+
+    elsif !!@right && @value < val
+      @right.find(val)
+
+    else
+      return nil
+
+    end
+
+  end
+
+  def no_parent?
+    if (parent == nil)
+      return true
+    else
+      return false
+    end
+  end
+
+    def no_children?
+    if (left == nil && right == nil)
+      return true
+    else
+      return false
+    end
+  end
+
+  def one_child?
+    if ((left != nil && right == nil) || 
+        (right != nil && left == nil))
+      return true
+    else
+      return false
+    end
+  end
+
+
+
+
 end
