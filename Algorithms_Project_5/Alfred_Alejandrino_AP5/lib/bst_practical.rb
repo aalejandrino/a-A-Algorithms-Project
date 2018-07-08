748a95388dbb5050748e6e348ba5e89ae73da603
AdoboFrenzy <alejandrino.alfred@gmail.com>
2018-07-08 12:40:59 -0700

    finish algorithms project 5

--- Algorithms_Project_5/Alfred_Alejandrino_AP5/lib/bst_practical.rb
+++ Algorithms_Project_5/Alfred_Alejandrino_AP5/lib/bst_practical.rb
@@ -1,3 +1,18 @@
 
 def kth_largest(tree_node, k)
+    traverse = in_order_node_traversal(tree_node)
+    traverse[traverse.length - k]
+end
+
+def in_order_node_traversal(tree_node = @root, arr = [])
+    return [] if tree_node == nil
+
+    result = []
+
+    result.concat in_order_node_traversal(tree_node.left)
+    result << tree_node
+    result.concat in_order_node_traversal(tree_node.right)
+
+    result
+
 end
