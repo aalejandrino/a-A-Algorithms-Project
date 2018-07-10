4445fc67e742e00ac1bed02c0cea16722954d53b
AdoboFrenzy <alejandrino.alfred@gmail.com>
2018-07-09 18:01:32 -0700

    include notes from lecture

--- Algorithms_Project_5/Alfred_Alejandrino_AP5/lib/bst_lecture.rb
+++ Algorithms_Project_5/Alfred_Alejandrino_AP5/lib/bst_lecture.rb
@@ -139,15 +139,24 @@ end
 
 # =========
 def kth_largest(tree_node, k)
-    kth_node = { count: 0, correct_node: nil }
-    reverse_inorder(tree_node, kth_node, k)[:correct_node]
+  kth_node = { count: 0, correct_node: nil }
+  reverse_inorder(tree_node, kth_node, k)[:correct_node]
 end
 
 def reverse_inorder(tree_node, kth_node, k)
-    if tree_node && kth_node[:count] < k
-        kth_node = reverse_inorder(tree_node.right, kth_node, k)
-        if kth_node[:count] < k
-            k
+  if tree_node && kth_node[:count] < k
+    kth_node = reverse_inorder(tree_node.right, kth_node, k)
+    if kth_node[:count] < k
+      kth_node[:count] += 1
+      kth_node[:correct_node] = tree_node
+    end
+
+    if kth_node[:count] < k
+      kth_node = reverse_inorder(tree_node.left, kth_node, k)
+    end
+  end
+
+  kth_node
 end
 
 #Trevor will slack out
@@ -160,4 +169,19 @@ def lowest_common_ancestor(tree_node = root, node_1, node_2)
     else
         return tree_node
     end
+end
+
+def lca(tree_node, node1, node2)
+  smaller = node1.value < node2.value ? node1.value : node2.value
+  bigger = node1.value > node2.value ? node1.value : node2.value
+
+  if tree_node.value >= smaller && tree_node.value <= bigger
+    lca = tree_node
+  elsif tree_node.value > smaller && tree_node.value > bigger
+    lca = lca(tree_node.left, node1, node2)
+  elsif tree_node.value < smaller && tree_node.value < bigger
+    lca = lca(tree_node.right, node1, node2)
+  end
+
+  lca
 end
