d5cba0d81879bda54dfce74e32de031f8fe79343
AdoboFrenzy <alejandrino.alfred@gmail.com>
2018-07-10 00:12:04 -0700

    finish install order problem

--- Algorithms_Project_6/Alfred_Alejandrino_AP6/lib/install_order.rb
+++ Algorithms_Project_6/Alfred_Alejandrino_AP6/lib/install_order.rb
@@ -7,8 +7,24 @@
 
 # Import any files you need to
 
-
+require_relative 'topological_sort'
 
 def install_order(arr)
+    max_id = arr.map{|s_arr| s_arr.first}.max
 
+    packages = (1..max_id).map {|val| Vertex.new(val)}
+
+    arr.each do |package_id, dependency|
+        Edge.new(packages[dependency - 1], packages[package_id - 1])
+    end
+
+    # ============================================================================
+    # p packages.map {|package| package.value}
+    # p packages.map {|package| package.in_edges.map{|edge| edge.from_vertex.value}}
+    # p packages.map {|package| package.out_edges.map{|edge| edge.to_vertex.value}}
+    #
+    # ** to test out vertices and edges **
+    # =============================================================================
+
+    return topological_sort(packages).map {|package| package.value}
 end
