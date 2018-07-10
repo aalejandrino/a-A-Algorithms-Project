a3c30abc59a08fdae1a9521d3a15bf8fbfff4474
AdoboFrenzy <alejandrino.alfred@gmail.com>
2018-07-09 23:32:24 -0700

    finish topological sort problems

--- Algorithms_Project_6/Alfred_Alejandrino_AP6/lib/graph.rb
+++ Algorithms_Project_6/Alfred_Alejandrino_AP6/lib/graph.rb
@@ -1,15 +1,42 @@
 class Vertex
-  def initialize(value)
+  attr_accessor :value, :in_edges, :out_edges
 
+  def initialize(value)
+    @value = value
+    @in_edges = []
+    @out_edges = []
+  end
+
+  def add_in_edge(edge)
+    @in_edges << edge
+  end
+
+  def add_out_edge(edge)
+    @out_edges << edge
   end
 end
 
 class Edge
-  def initialize(from_vertex, to_vertex, cost = 1)
+  attr_accessor :from_vertex, :to_vertex, :cost
 
+  def initialize(from_vertex, to_vertex, cost = 1)
+    @from_vertex = from_vertex
+    @from_vertex.add_out_edge(self)
+
+    @to_vertex = to_vertex
+    @to_vertex.add_in_edge(self)
+
+    @cost = cost
   end
 
   def destroy!
+    # @from_vertex.out_edges.delete(self) #doesn't delete properly??
+    @from_vertex.out_edges -= [self]
+    self.from_vertex = nil
+
+    # @to_vertex.in_edges.delete(self)
+    @to_vertex.in_edges -= [self]
+    self.to_vertex = nil
 
   end
 end
--- Algorithms_Project_6/Alfred_Alejandrino_AP6/lib/topological_sort.rb
+++ Algorithms_Project_6/Alfred_Alejandrino_AP6/lib/topological_sort.rb
@@ -1,7 +1,34 @@
 require_relative 'graph'
+require 'byebug'
 
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
 def topological_sort(vertices)
+    sorted = []
+    top = Queue.new
 
+    vertices.each do |vertex|
+        if vertex.in_edges.empty?
+            top.enq(vertex)
+        end
+    end
+
+    until top.empty?
+        current_node = top.pop
+        sorted << current_node
+        # byebug
+        current_node.out_edges.each do |edge|
+            next_node = edge.to_vertex
+            edge.destroy!
+            
+            if next_node.in_edges.empty?
+                top.enq(next_node)
+            end
+
+        end
+
+    end
+    
+    # p sorted.map {|node| node.value}
+    sorted.length == vertices.length ? sorted : []
 end
